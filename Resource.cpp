3a5d9b1434172119bafbb579ece769f30379bb9d
lamburk <john.constantine365@gmail.com>
2021-12-17 12:47:37 +0530

    First commit

---
+++ Resource.cpp
@@ -0,0 +1,109 @@
+//
+// Created by peterparker on 15-12-2021.
+//
+
+// All the functions given in the Header.h file must be implemented here
+// A brief synopsis of what these functions do is mentioned
+
+#include "Header.h"
+
+string convertToLower(string A)
+{
+
+}
+int AVLTree::height(AVL *temp)
+{
+
+}
+int AVLTree::max(int x, int y)
+{
+
+}
+int AVLTree::diff(AVL *temp)
+{
+
+}
+void AVLTree::RR(AVL *&root)
+{
+
+}
+void AVLTree::LL(AVL *&root)
+{
+
+}
+void AVLTree::LR(AVL *&root)
+{
+
+}
+void AVLTree::RL(AVL *&root)
+{
+
+}
+void AVLTree::balance(AVL *&root)
+{
+
+}
+void AVLTree::insertByMain(string word, int &count, ofstream &out, int &result)
+{
+
+}
+void AVLTree::insert(AVL *&root, string value, int &count, ofstream &out, int &result)
+{
+
+    //return root;
+
+}
+bool AVLTree::find(AVL *&root, string A, ofstream &out)
+{
+
+}
+AVL *AVLTree::findMin(AVL *&temp)
+{
+
+}
+void AVLTree::remove(string x, AVL *&root)
+{
+
+}
+void spellChecker::inputFromFile(ifstream &in, ofstream &out)
+{
+
+}
+AVL* AVLTree::getRoot()								//iterator
+{
+
+}
+void spellChecker::commandFile(ofstream &out, ifstream &in)
+{
+
+
+}
+void linkList::display(node *&tempH, ofstream &out)
+{
+
+}
+void spellChecker::insert(string A, ofstream &out, int &result)
+{
+
+}
+void spellChecker::check(AVL *&root, string A)
+{
+
+}
+void linkList::deleteList(node *&tempH)
+{
+
+}
+void linkList::insert(node *& tempH, string A)
+{
+
+}
+void spellChecker::inOrderByMain(string A, ofstream &out)
+{
+
+}
+void spellChecker::inOrder(AVL *&root, ofstream &out, int &height)
+{
+
+
+}
---
+++ Source.cpp
@@ -0,0 +1,33 @@
+//
+// Created by peterparker on 15-12-2021.
+//
+#include "Header.h"
+void main(int argc,char** argv)
+{
+    ifstream inD,inC;
+    ofstream outLog;
+    string A;
+    if (argc >= 4)
+    {
+        inD.open(argv[2]);
+        if (inD.is_open())
+        {
+            inC.open(argv[1]);
+            if (inC.is_open())
+            {
+                outLog.open(argv[3]);
+                spellChecker obj;
+                obj.inputFromFile(inD,outLog);
+                obj.commandFile(outLog, inC);
+                outLog.close();
+                inC.close();
+            }
+            else
+                cout << "Error: Command File is Missing.";
+            inD.close();
+        }
+        cout <<  "Error: Command File is Missing.";
+
+    }
+
+}
